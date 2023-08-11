bf24c7e175136dda7fd1f6da61e15b50294a2b0f
higuro-test <rkf0645@google.com>
2023-08-11 13:58:43 +0900

    test0811

---
+++ test/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication_radiogroup;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    RadioButton rb1, rb2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rg = findViewById(R.id.radioGroup01);
+        rb1 = findViewById(R.id.radio01);
+        rb2 = findViewById(R.id.radio02);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.indexOfChild(findViewById(checkedId))) {
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "1st check!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "2nd check!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+
+    public void onSelectButtonClicked(View view) {
+        if (rb1.isChecked() == true) {
+            Toast.makeText(getApplicationContext(), "남성", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "여성", Toast.LENGTH_LONG).show();
+        }
+    }
+}
